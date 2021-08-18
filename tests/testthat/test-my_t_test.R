62c9dfe88db0dc77c27bbbab53b6e57dd14cfc44
David <david.zeng@protonmail.com>
2021-08-18 14:23:03 -0700

    Created initial test case for test-my_t_test.R

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -3,8 +3,17 @@
 # create input data for tests
 x <- rnorm(100, mean = 0, sd = 1)
 
+# create correct outputs
+result_less_0 <- list("test statistic" = 0.9190272,
+                      "degrees of freedom" = 99,
+                      "alternative" = "less",
+                      "p-value" = 0.8198425)
 test_that("error message gets thrown for invalid input", {
   expect_error(my_t.test(x, "twosided", 0))
   expect_error(my_t.test(x, "less than", 0 ))
   expect_error(my_t.test(x, "greater than", 0 ))
+})
+
+test_that("output is correct for given test cases", {
+  expect_equal(my_t.test(x, "less", 0))
 })
