470d7863d55dfa6658125b1b3ee71a1ee587ba9f
David <david.zeng@protonmail.com>
2021-08-18 14:37:46 -0700

    Fixed test case error in test_my_t_test.R

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -3,11 +3,6 @@
 # create input data for tests
 x <- rnorm(100, mean = 0, sd = 1)
 
-# create correct outputs
-output_less_0 <- list("test statistic" = 0.9190272,
-                      "degrees of freedom" = 99,
-                      "alternative" = "less",
-                      "p-value" = 0.8198425)
 test_that("error message gets thrown for invalid input", {
   expect_error(my_t.test(x, "twosided", 0))
   expect_error(my_t.test(x, "less than", 0 ))
@@ -15,5 +10,8 @@ test_that("error message gets thrown for invalid input", {
 })
 
 test_that("output is correct for given test cases", {
-  expect_equal(my_t.test(x, "less", 0), output_less_0)
+  expect_equal(my_t.test(x, "two.sided", 0)$'p-value', t.test(x, mu = 0, alternative="two.sided")$'p.value')
+  expect_equal(my_t.test(x, "less", 0)$'p-value', t.test(x, mu = 0, alternative="less")$'p.value')
+  expect_equal(my_t.test(x, "greater", 0)$'p-value', t.test(x, mu = 0, alternative="greater")$'p.value')
+
 })
